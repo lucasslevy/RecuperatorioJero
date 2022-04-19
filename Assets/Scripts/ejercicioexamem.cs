1f33fe9b8de28fe3cf5c6e0c5cbf5dd8e37bbc95
lucasslevy <levylulo@gmail.com>
2022-04-19 11:57:37 -0300

    recuperatorio

---
+++ Assets/Scripts/ejercicioexamem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicioexamem : MonoBehaviour
+
+{
+    public int EdadDeUsuario;
+    public string MiTexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (EdadDeUsuario >= 18)
+        {
+            Debug.Log("es mayor de edad");
+        }
+
+    
+    
+        else
+    {
+        Debug.Log("es menor de edad");
+    }
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/exercicio1.cs
+++ Assets/Scripts/exercicio1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EJercicioExamen : MonoBehaviour
+public class exercicio1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
